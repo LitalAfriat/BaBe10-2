5cb6b89652f89c29cede7828c3a25c5b8df30558
LitalAfriat <afriatlital@gmail.com>
2025-09-22 10:39:59 +0300

    fixSerch

--- components/menu/Search.jsx
+++ components/menu/Search.jsx
@@ -1,13 +1,12 @@
-import { View, TextInput } from "react-native";
-import React, { forwardRef } from "react";
 import { Ionicons } from "@expo/vector-icons";
-
-export default Search = forwardRef(({func,focus = false},ref) => {
+import { forwardRef } from "react";
+import { TextInput, View } from "react-native";
+export default forwardRef(function Search({ func, focus = false }, ref) {
     return (
         <View>
             {/* Search bar */}
             <View
-                className='mx-6 px-6 p-4 rounded-full flex flex-row  justify-end items-center '
+                className="mx-6 px-6 p-4 rounded-full flex flex-row  justify-end items-center "
                 style={{
                     shadowColor: "#727273",
                     shadowRadius: 4,
@@ -17,23 +16,24 @@ export default Search = forwardRef(({func,focus = false},ref) => {
                     flexDirection: "row",
                     alignItems: "center",
                     helight: 48,
-                }}>
+                }}
+            >
                 {/* The string that will be rendered before text input has been entered. */}
 
                 <TextInput
                     style={{ flex: 1 }}
-                    className='mr-2  text-[18px] text-right	'
-                    placeholder='חיפוש'
+                    className="mr-2  text-[18px] text-right	"
+                    placeholder="חיפוש"
                     ref={ref}
-                    placeholderTextColor='gray'
+                    placeholderTextColor="gray"
                     onChangeText={(value) => {
-                        func(value)
+                        func(value);
                     }}
                     autoFocus={focus}
                     rtl
                 />
-                <Ionicons name='search' size={24} color='gray' />
+                <Ionicons name="search" size={24} color="gray" />
             </View>
         </View>
     );
-})
+});
